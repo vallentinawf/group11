8df03ce525d119de7455ef617831904cd8b10ba1
akbarsigit <akbarsigit3@gmail.com>
2022-09-25 20:07:42 +0700

    pull update

--- controllers/rentalController.js
+++ controllers/rentalController.js
@@ -14,57 +14,63 @@ exports.createRental = catchAsync(async (req, res, next) => {
   });
 });
 
-exports.update =(req,res) => {
-  if(!req.body){
+exports.update = (req, res) => {
+  if (!req.body) {
     return res.status(400).send({
-      message: "Data to update can not be empty."
-    })
+      message: 'Data to update can not be empty.'
+    });
   }
   const id = req.params.id;
-  Rental.findByIdAndUpdate(id, req.body, {useFindAndModify: false})
-  .then(data => {
-    if (!data){
-      res.status(404).send({
-        message: `Cannot update Rental with id=${id}.`
+  Rental.findByIdAndUpdate(id, req.body, { useFindAndModify: false })
+    .then(data => {
+      if (!data) {
+        res.status(404).send({
+          message: `Cannot update Rental with id=${id}.`
+        });
+      } else res.send({ message: 'Rental was updated successfully.' });
+    })
+    .catch(err => {
+      res.status(500).send({
+        message: 'Error updating Rental with id=' + id
       });
-    } else res.send({message: "Rental was updated successfully."});
-  })
-  .catch(err => {
-    res.status(500).send({
-      message:"Error updating Rental with id=" +id
     });
-  });
 };
 
 exports.deleteRental = async (req, res, next) => {
-  try{
-      console.log(req.params.id)
-      // req.params.id = req.params.id.trim()
-      // console.log(req.params.id)
-      const requestedDelete = await Rental.findByIdAndDelete(req.params.id);
-      if(!requestedDelete){
-          return next(new ErrorResponse(`Cannot find modules with id of ${req.params.id}`, 404));
-      }
-      res.status(200).json({success: true, data: {}});
-  }catch (e) {
-      next(e);
+  try {
+    console.log(req.params.id);
+    // req.params.id = req.params.id.trim()
+    // console.log(req.params.id)
+    const requestedDelete = await Rental.findByIdAndDelete(req.params.id);
+    if (!requestedDelete) {
+      return next(
+        new ErrorResponse(
+          `Cannot find modules with id of ${req.params.id}`,
+          404
+        )
+      );
+    }
+    res.status(200).json({ success: true, data: {} });
+  } catch (e) {
+    next(e);
   }
-}
+};
 
 exports.findAll = (req, res) => {
   const title = req.query.title;
-  var condition = title ? { title: { $regex: new RegExp(title), $options: "i"}} : {};
+  var condition = title
+    ? { title: { $regex: new RegExp(title), $options: 'i' } }
+    : {};
 
   Rental.find(condition)
-  .then(data => {
-    res.send(data);
-  })
-  .catch(err => {
-    res.status(500).send({
-      message:
-      err.message || "Some error occured while retrieving rentals."
+    .then(data => {
+      res.send(data);
+    })
+    .catch(err => {
+      res.status(500).send({
+        message: err.message || 'Some error occured while retrieving rentals.'
+      });
     });
-  });
 };
 
 exports.findById = async (req, res, next) => {
@@ -72,7 +78,7 @@ exports.findById = async (req, res, next) => {
 
   Rental.findById(id)
     .then(data => {
-      if (!data){
+      if (!data) {
         res.status(404).send({
           message: `Cannot find Rental with id= ${id}`
         });
@@ -82,6 +88,5 @@ exports.findById = async (req, res, next) => {
       res.status(500).send({
         message: err.message
       });
-    });  
+    });
 };
-
