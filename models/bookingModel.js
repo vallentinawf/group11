a46c629fd3af0136acaa40add3cab20cdce29571
Muhammad Arif Hidayat <mhmdarif.developer@gmail.com>
2022-09-30 10:17:52 +0700

    feat(bookingModel) : add bookingModel

---
+++ models/bookingModel.js
@@ -0,0 +1,26 @@
+const mongoose = require('mongoose');
+
+const bookingSchema = new mongoose.Schema({
+  rentalId: {
+    type: mongoose.Types.ObjectId,
+    required: true,
+    unique: false
+  },
+  userId: {
+    type: mongoose.Types.ObjectId,
+    required: true,
+    unique: false
+  },
+  bookingAt: {
+    type: Date,
+    required: false
+  },
+  Returned: {
+    type: Boolean,
+    required: true,
+    default: false
+  }
+});
+
+const Booking = mongoose.model('Booking', bookingSchema);
+module.exports = Booking;
