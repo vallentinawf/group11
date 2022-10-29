81f6ea2b2f5215e28fbef7bd2e90867758bafda4
Vallentina Wahyu Febrihartanti <vallentina.wahyu15@gmail.com>
2022-10-29 21:49:05 +0700

    Create react environment and login page

---
+++ frontend/src/components/Login.js
@@ -0,0 +1,40 @@
+import * as React from 'react';
+import loginImg from '../assets/login.jpeg';
+import Logo from '../assets/logo.png';
+
+export default function Login() {
+    return(
+    <div className='grid grid-cols-1 sm:grid-cols-2 h-screen w-full bg-gray'>
+        <div className='bg-gray flex flex-col justify-center'>
+            <form className='max-w-[500px] bg-gray w-full mx-auto px-10 p-10'>
+                <img className='object-scale-down w-6/12' src={Logo} alt='Logo'></img>
+                <p className='text-center font-bold p-2 text-black/75'>Login into your account</p>
+                <div className='flex flex-col py-2 px-5'>
+                    <label className='text-black/50'>Email address</label>
+                    <input className='p-2 w-full rounded-lg' type="text"></input>
+                </div>
+                <div className='flex flex-col py-2 px-5'>
+                    <label className='text-black/50'>Password</label>
+                    <input className='p-2 w-full rounded-lg' type="password"></input>
+                </div>
+                <div className='text-center'>
+                    <button className='text-blue hover:text-darkblue underline'>Forgot password?</button>
+                </div>
+                <div className='px-5'>
+                    <button className='w-full my-5 py-2 bg-orange text-white font-bold rounded-lg shadow-md shadow-orange/60 hover:shadow-orange/40 '>Login now</button>
+                </div>
+                <div className='text-center text-black/25'>
+                    OR
+                </div>
+                <div className='px-5'>
+                    <button className='border w-full my-5 py-2 font-bold text-orange rounded-lg'>Signup now</button>
+                </div>
+            </form>
+        </div>
+
+        <div className='hidden sm:block'>
+            <img className='w-full h-full object-cover' src={loginImg} alt="" />
+        </div>
+    </div>
+    )
+}
---
+++ frontend/tailwind.config.js
@@ -0,0 +1,20 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
+  theme: {
+    colors:{
+      transparent:'transparent',
+      current:'currentColor',
+      darkblue:'#14213D',
+      orange:'#FCA311',
+      gray:'#E5E5E5',
+      blue:'#1E2772',
+      white:'#FFFFFF',
+      black:'#000000',
+    },
+    extend: {},
+  },
+  plugins: [],
+}
