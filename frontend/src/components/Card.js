8d83a0844dfb23477d235a376025623c85a8d079
Akbar Sigit P <akbarsigit3@gmail.com>
2022-11-11 11:28:10 +0700

    add route config

---
+++ frontend/src/components/Card.js
@@ -0,0 +1,10 @@
+import * as React from 'react';
+import Logo from '../assets/logoTransparent.png';
+
+export default function Card() {
+    return(
+        <div className='w-screen h-[80px] bg-white fixed drop-shadow-lg'>
+           
+        </div>
+    )
+}
