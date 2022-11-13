bab53e98592f2a9062571620d670bd3f63935d60
rangga.a.r <rangga.a.r@mail.ugm.ac.id>
2022-11-13 16:13:58 +0700

    refactor(auth): added try catch, added route

--- backend/routes/authRoute.js
+++ backend/routes/authRoute.js
@@ -10,7 +10,10 @@ router.post('/register', authController.register);
 router.post('/login', authController.login);
 //Forgot Password
 router.post('/forgotpassword', authController.forgotPassword);
+//Reset Password
+router.put('/resetpassword/:resettoken',authController.resetPassword)
 //logout
 router.post('/logout', authController.logout);
+
 
 module.exports = router;
